b78c9e1ab417fa6e4d90dd66586cf1a10b87f402
dhrihoryev <dmitry.hrihoryev@nure.ua>
2017-05-02 22:27:13 +0300

    Created view for Login and Register

---
+++ Library/Library/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            ViewBag.Title = "Sign-In";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Title = "Sign-Up";
+
+            return View();
+        }
+    }
+}
